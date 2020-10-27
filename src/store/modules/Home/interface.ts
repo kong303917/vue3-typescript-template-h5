8c39cfc62c6f7bf1ee67c9a8aabc2edb918b81d9
zhoukongcheng <zhoukongcheng@zhoukongchengdeMacBook-Air.local>
2020-10-27 16:39:55 +0800

    init

---
+++ src/store/modules/Home/interface.ts
@@ -0,0 +1,24 @@
+export interface ICity {
+  cityName: string,
+  cityId: string,
+  communityId: string,
+  communityName: string,
+  provinceName: string,
+  districtName: string
+}
+
+export interface IAccessControl {
+  doorControlId: string,
+  doorControlName: string
+}
+
+export interface IHomeState {
+  cityList: ICity[],
+  commonlyUsedDoor: ICommonlyUsedDoor,
+  accessControlList: IAccessControl[]
+}
+
+export interface ICommonlyUsedDoor{
+  doorControlId: string,
+  doorControlName: string
+}
